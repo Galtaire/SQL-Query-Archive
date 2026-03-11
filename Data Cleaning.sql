3113323e6ec706b2d5f0c4d39401edbb695b6c30
Galtaire <tan.villegas.20@gmail.com>
2026-03-11 14:38:03 +0800

    Update Data Cleaning.sql

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -1,4 +1,4 @@
-### 1. Duplicate Marker and Identifier
+######### 1. Duplicate Marker and Identifier
 -- 			Allows you to assign a number to duplicates based on the column selected in the (PARTITION BY ). 
 -- 			Unique rows will have 1 and duplicates will have the number 2 or 3. 
 -- 			Use CTE to identify which rows are the duplicates 
@@ -17,7 +17,7 @@ WHERE "Alias" > 1;
 
 
 
-### 2. Identify Case-Sensitive Duplicates 
+######### 2. Identify Case-Sensitive Duplicates 
 -- 			Identifies Case-sensitive duplicates like Google and google or McDonalds and Mcdonalds
 
 SELECT `column1`, COUNT(*) AS Alias
@@ -28,7 +28,7 @@ HAVING COUNT(DISTINCT BINARY `column1`) > 1;
 
 
 
-### 3. Self-Join to update missing values based on the table itself
+######### 3. Self-Join to update missing values based on the table itself
 -- 			fills missing values using the table itself
 -- 			column1 is the priamry matching point of both tables (ex. company)
 -- 			column2 is the secondary matching point of both tables (ex. location)
@@ -56,11 +56,11 @@ WHERE t1.column3 IS NULL
 
 
 
-### 4. Adding a Primary Key + Column to the staging table.
+######### 4. Adding a Primary Key + Column to the staging table.
 -- Use FIRST after PRIMARY KEY instead of putting the table name. This makes the column as the first column in the table.
 -- Note: In this case, no need to put the column name aafter FIRST. This already makes the generated column first. (best for making Primary Keys) 
 
-	ALTER TABLE table_name
+ALTER TABLE table_name
 ADD `column_name` data_type
 PRIMARY KEY FIRST;
 
@@ -73,7 +73,13 @@ PRIMARY KEY AFTER `column2_name`;
 
 
 
+######### 5.  Extracting a substring of a string before a specified number of delimiter occurs.
 
+-- Delimiter: The delimiter to search for (-_/|\:; etc)
+-- Number: The number of times to search for the delimiter. Can be both a positive or negative number.
+	--  If it is a positive number, this function returns all to the left of the delimiter. If it is a negative number, this function returns all to the right of the delimiter.
+
+SELECT SUBSTRING_INDEX(`string or column_name`, delimiter, number)
 
 
 
