2518f4fefb2ba4ec9924fb7d88380629b8db734b
Galtaire <tan.villegas.20@gmail.com>
2026-03-12 04:16:01 +0800

    Update Data Cleaning.sql

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -1,8 +1,10 @@
+###########################################################################################################################################################################
+
 ######### 1. Duplicate Marker and Identifier
 -- 			Allows you to assign a number to duplicates based on the column selected in the (PARTITION BY ). 
 -- 			Unique rows will have 1 and duplicates will have the number 2 or 3. 
 -- 			Use CTE to identify which rows are the duplicates 
-
+	
 SELECT *,
 ROW_NUMBER() OVER(PARTITION BY column1, column2, etc ) AS Alias
 FROM `table_name`;
@@ -15,7 +17,7 @@ SELECT  *
 FROM `cte_name`
 WHERE "Alias" > 1;
 
-
+###########################################################################################################################################################################
 
 ######### 2. Identify Case-Sensitive Duplicates 
 -- 			Identifies Case-sensitive duplicates like Google and google or McDonalds and Mcdonalds
@@ -25,8 +27,7 @@ FROM `table_name`
 GROUP BY `column1`
 HAVING COUNT(DISTINCT BINARY `column1`) > 1;
 
-
-
+###########################################################################################################################################################################
 
 ######### 3. Self-Join to update missing values based on the table itself
 -- 			fills missing values using the table itself
@@ -54,7 +55,7 @@ SET t1.column3 = t2.column3
 WHERE t1.column3 IS NULL
 	AND t2.column3 IS NOT NULL;
 
-
+###########################################################################################################################################################################
 
 ######### 4. Adding a Primary Key + Column to the staging table.
 -- Use FIRST after PRIMARY KEY instead of putting the table name. This makes the column as the first column in the table.
@@ -71,7 +72,7 @@ ALTER TABLE table_name
 ADD `column1_name` data_type
 PRIMARY KEY AFTER `column2_name`;
 
-
+###########################################################################################################################################################################
 
 ######### 5.  Extracting a substring of a string before a specified number of delimiter occurs.
 
@@ -80,6 +81,34 @@ PRIMARY KEY AFTER `column2_name`;
 	--  If it is a positive number, this function returns all to the left of the delimiter. If it is a negative number, this function returns all to the right of the delimiter.
 
 SELECT SUBSTRING_INDEX(`string or column_name`, delimiter, number)
+
+###########################################################################################################################################################################
+
+######### 6. SUBSTRING_INDEX + LOCATE - A precise way to "cut" your text data to isolate a specific substring/character from a string
+-- This combination allows you to be more precise in extracting a subtring
+		-- LOCATE(substring, string, start) or  LOCATE(value, column, start)= returns the number position of the "Substring" from a "String". "Start" is 1 in default
+		-- LOCATE("comment", "full_comment") returns 6 because "c" in "full_comment" is the 6th character in the string. 
+		-- Value can be wildcards, strings, numbers, etc. 
+
+-- SUBSTRING(string, start, length)
+-- LOCATE(substring, string, start)
+	
+SELECT SUBSTRING(vessel_key, LOCATE('_', vessel_key) + 1) 
+
+-- SELECT SUBSTRING(`column_name`, LOCATE(`substring`, `string`), length)
+-- This basically means that LOCATE(`substring`, `string`) becomes the "start" value of Substring because LOCATE will return a position number of a string
+
+	-- LENGTH value meaning:
+		-- +1 = Jumps over the character found
+		-- 1 = Grabs the first piece from the left
+		-- -1 = Grabs the last piece from the right.
+
+###########################################################################################################################################################################
+
+
+
+
+
 
 
 
